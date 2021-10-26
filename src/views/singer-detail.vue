223ba536d683a6046c81c09cca8cc1166a7a5d59
lzcdev <lzc2134@gmail.com>
2021-10-26 16:58:45 +0800

    feat: music-list组件

--- src/views/singer-detail.vue
+++ src/views/singer-detail.vue
@@ -2,27 +2,47 @@
  * @Author: jinqing
  * @Date: 2021-10-22 14:37:04
  * @LastEditors: jinqing
- * @LastEditTime: 2021-10-22 15:53:27
+ * @LastEditTime: 2021-10-26 16:14:26
  * @Description: singer-detail
 -->
 
 <template>
-  <div class='singer-detail'></div>
+  <div class='singer-detail'>
+    <music-list :songs='songs' :title='title' :pic='pic' :loading='loading'></music-list>
+  </div>
 </template>
 
 <script>
 import { getSingerDetail } from '@/service/singer'
 import { processSongs } from '@/service/song'
+import MusicList from '@/components/music-list/music-list'
 
 export default {
   name: 'singer-detail',
+  components: {
+    MusicList
+  },
   props: {
     singer: Object
   },
+  data() {
+    return {
+      songs: [],
+      loading: true
+    }
+  },
+  computed: {
+    pic() {
+      return this.singer && this.singer.pic
+    },
+    title() {
+      return this.singer && this.singer.name
+    }
+  },
   async created() {
     const result = await getSingerDetail(this.singer)
-    const songs = await processSongs(result.songs)
-    console.log('rrrr====>', songs)
+    this.songs = await processSongs(result.songs)
+    this.loading = false
   }
 
 }
