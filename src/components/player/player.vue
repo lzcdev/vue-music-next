ea40d662343ef4be7a47acc99bfa947e00b943d4
lzcdev <lzc2134@gmail.com>
2021-11-02 16:19:17 +0800

    feat: 歌曲播放与暂停

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,7 +2,7 @@
  * @Author: jinqing
  * @Date: 2021-11-01 19:11:59
  * @LastEditors: jinqing
- * @LastEditTime: 2021-11-01 19:50:22
+ * @LastEditTime: 2021-11-02 16:18:21
  * @Description: player
 -->
 
@@ -13,14 +13,33 @@
         <img :src='currentSong.pic' alt />
       </div>
       <div class='top'>
-        <div class='back'>
+        <div class='back' @click='goBack'>
           <i class='icon-back'></i>
         </div>
         <h1 class='title'>{{currentSong.name}}</h1>
         <h2 class='subtitle'>{{currentSong.singer}}</h2>
       </div>
+      <div class='bottom'>
+        <div class='operators'>
+          <div class='icon i-left'>
+            <i class='icon-sequence'></i>
+          </div>
+          <div class='icon i-left'>
+            <i class='icon-prev'></i>
+          </div>
+          <div class='icon i-center' @click='togglePlay'>
+            <i :class='playIcon'></i>
+          </div>
+          <div class='icon i-right'>
+            <i class='icon-next'></i>
+          </div>
+          <div class='icon i-right'>
+            <i class='icon-not-favorite'></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <audio ref='audioRef'></audio>
+    <audio ref='audioRef' @pause='pause'></audio>
   </div>
 </template>
 <script>
@@ -34,7 +53,10 @@ export default {
     const store = useStore()
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
-
+    const playing = computed(() => store.state.playing)
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return
@@ -45,10 +67,31 @@ export default {
       audioEl.play()
     })
 
+    watch(playing, (newPlaying) => {
+      const audioElement = audioRef.value
+      newPlaying ? audioElement.play() : audioElement.pause()
+    })
+
+    const goBack = () => {
+      store.commit('setFullScreen', false)
+    }
+
+    const togglePlay = () => {
+      store.commit('setPlayingState', !playing.value)
+    }
+
+    const pause = () => {
+      store.commit('setPlayingState', false)
+    }
+
     return {
+      playIcon,
       audioRef,
       fullScreen,
-      currentSong
+      currentSong,
+      goBack,
+      togglePlay,
+      pause
     }
   }
 }
@@ -108,6 +151,41 @@ export default {
         text-align: center;
         font-size: $font-size-medium;
         color: $color-text;
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
       }
     }
   }
